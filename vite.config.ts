3aa822d966873ad37578b49a46417945fa98f868
kks <saway126@users.noreply.github.com>
2025-09-06 15:03:31 +0900

    fix: Fix GitHub Actions workflow for Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/greenwear/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
